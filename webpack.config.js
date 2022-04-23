fa262f2c54474e4947a3ced3e3aa90fab1854f71
Mik39sch <mlucky39sch@gmail.com>
2022-04-23 09:23:22 +0900

    initialize

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const CopyFilePlugin = require("copy-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.min.js'
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+        terserOptions: {
+            ecma: 6,
+            compress: true,
+            output: {
+                comments: false,
+                beautify: false
+            }
+        }
+      })]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'JS13kgames2022d',
+            template: './index.html',
+            filename: 'index.html',
+        }),
+        new CopyFilePlugin({
+            patterns: [
+                {
+                    context: "assets",
+                    from: "**/*.*",
+                    to: __dirname + "/dist/assets"
+                }
+            ],
+            // { copyUnmodified: true }
+        }),
+        new WriteFilePlugin()
+    ],
+};
